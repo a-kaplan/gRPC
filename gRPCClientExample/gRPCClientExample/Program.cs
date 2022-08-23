8a28d7521053b710e6c9f77b8780a61d71f6a8b6
AKIN KAPLAN <akinkaplan.com.tr@gmail.com>
2022-08-24 00:52:58 +0300

    first gRPC exm

---
+++ gRPCClientExample/gRPCClientExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPCClientExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+
+                var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var greetClient = new Greeter.GreeterClient(channel);
+                HelloReply response = await greetClient.SayHelloAsync(new HelloRequest { Name = "hi say" });
+                Console.WriteLine($"Gelen Cevap 1: {response.Message}");
+
+                response = await greetClient.SayHelloAsync(new HelloRequest { Name = "hi say 2" });
+                Console.WriteLine($"Gelen Cevap 2: {response.Message}");
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
